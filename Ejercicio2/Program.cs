b50435769413944c81d1ff2e1ce1304b5ad441d0
emybarr <eybarreiro.2a@gmail.com>
2022-06-28 15:19:29 -0300

    tp9

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -10,6 +10,7 @@ namespace Ejercicio2
         {
            int cant; 
            List<productos> listProductos = new List<productos>();
+           var manejoArchivos = new manejoArchivo();
          
            Console.WriteLine("Ingrese la cantidad de productos : ");
            cant = Convert.ToInt32(Console.ReadLine());
@@ -21,26 +22,29 @@ namespace Ejercicio2
             Console.WriteLine("------------------------------------------");
             Console.WriteLine("Nombre : " + prod.Nombre);
             Console.WriteLine("Precio :" + prod.Precio);
-            Console.WriteLine("Fecha de Vencimeinto : " + prod.Fechadevencimiento);
+            Console.WriteLine("Fecha de Vencimeinto : " + prod.Fechadevencimiento.ToShortDateString());
             Console.WriteLine("Tamanio :" + prod.Tamanio);
 
            }
-           string archivonombre = "productos.json";
-           File.WriteAllText("productos.json",JsonSerializer.Serialize(listProductos));
-           
-           string jsonlista = File.ReadAllText(archivonombre);
-           listProductos = JsonSerializer..Deserialize<List<listproductos>>(jsonlista);
 
-           foreach(var p in listProductos){
-            Console.WriteLine("------------------------------------------");
-            Console.WriteLine("Nombre : " + prod.Nombre);
-            Console.WriteLine("Precio :" + prod.Precio);
-            Console.WriteLine("Fecha de Vencimeinto : " + prod.Fechadevencimiento);
-            Console.WriteLine("Tamanio :" + prod.Tamanio);
+           string archivo = "productos.json";
+           string produJson =JsonSerializer.Serialize(listProductos);
+           manejoArchivos.GuardarArchivoTexto(archivo,produJson);
 
+           string jsonP = manejoArchivos.AbrirArchivoTexto(archivo);
+           var listaJson = JsonSerializer.Deserialize<List<productos>>(jsonP);
+          
+            Console.WriteLine("------------------Lista Json------------------------");
+            foreach(productos p in listaJson){
+            Console.WriteLine("Nombre : " + p.Nombre);
+            Console.WriteLine("Precio :" + p.Precio);
+            Console.WriteLine("Fecha de Vencimeinto : " + p.Fechadevencimiento.ToShortDateString());
+            Console.WriteLine("Tamanio :" + p.Tamanio);
 
            }
-        
+         
 }
-}
+     
+        }
+  
 }
